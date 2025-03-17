67e384c57e9306f1ae1b6bbf11b2269acc593e32
Dan Kibwika <dkibwika@akillix.com>
2025-03-17 10:29:14 +0200

    Added new files

--- The Complete Flutter Development Bootcamp with Dart/0.Introduction_to_Flutter/untitled1/lib/main.dart
+++ The Complete Flutter Development Bootcamp with Dart/0.Introduction_to_Flutter/untitled1/lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-            // appBar: AppBar(
-            //   title:Title(
-            //       color: Colors.white,
-            //       child: Text("LearnGo"),) ,
-            //   backgroundColor: Colors.blue,
-            // ),
-            backgroundColor: Colors.white,
-            body: Center(
-              child: Image(
-                image:AssetImage("images/img1.png"),
-              ),
-            ),
+  MaterialApp(
+    home: Scaffold(
+      // appBar: AppBar(
+      //   title:Title(
+      //       color: Colors.white,
+      //       child: Text("LearnGo"),) ,
+      //   backgroundColor: Colors.blue,
+      // ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image(
+          image:AssetImage("images/img1.png"),
         ),
       ),
-    );
+    ),
+  ),
+);
