14ab1a8f6f0761e7c9501a923644e57eef86b0b3
Dan Kibwika <dkibwika@akillix.com>
2025-05-05 10:33:03 +0200

    update 23

--- Full-Stack Web Developer_PROJECT/SECTION14_JAVA_SCRIPT/DOME2/5.contsructot_function.js
+++ Full-Stack Web Developer_PROJECT/SECTION14_JAVA_SCRIPT/DOME2/5.contsructot_function.js
@@ -4,9 +4,12 @@ function HouseKeeper(name, age, yOe, skills){
     this.age = age;
     this.yOe = yOe;
     this.skills = skills;
+    this.startCleaning = function() {
+        alert("Let start cleaning !!! <3");
+    }
 
     return "hello my name is "+ name +" i am "+ age +" i have "+ yOe +"of experiences and my skills are "+ skills;
 }
-
+// examples 
 var houseKeeper1 = new HouseKeeper("Maria", "23", 23, ["Cook", "clean", "Iron"]);
 var houseKeeper2 = new HouseKeeper("Eden", "23", 23, ["Cook", "clean", "Iron", "other"]);
