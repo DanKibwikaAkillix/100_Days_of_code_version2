7c8eb2b42ad2c15f75dee29926518b518975cc36
Dan Kibwika <dkibwika@akillix.com>
2025-05-05 09:52:44 +0200

    updatev22

--- Full-Stack Web Developer_PROJECT/SECTION14_JAVA_SCRIPT/DOME2/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Full-Stack Web Developer_PROJECT/SECTION14_JAVA_SCRIPT/DOME2/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,12 +1,51 @@
-// function handleClick() {
-//     alert("i got clicked!");
-// }
 
-// document.querySelector("button.w").addEventListener("click", handleClick)
 
 var numbOfDrumButtons = document.querySelectorAll("button.drum").length;
-for(var i=0; i<numbOfDrumButtons; i++) {
-    document.querySelectorAll('button.drum')[i].addEventListener("click", function(){
-        alert("i got clicked!");
+for (var i = 0; i < numbOfDrumButtons; i++) {
+
+    document.querySelectorAll('button.drum')[i].addEventListener("click", function () {
+
+        var buttonInnerHtml = this.innerHTML;
+        switch (buttonInnerHtml) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+            case "j":
+                var crash= new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "k":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+
+            case "l":
+                var kick_bass = new Audio("sounds/kick-bass.mp3");
+                kick_bass.play();
+                break;
+
+            default: console.log(buttonInnerHtml);
+               
+        }
     })
 }
+
