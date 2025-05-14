ce1ba0914ec9425030a8ead7d670779241b151b6
Dan Kibwika <dkibwika@akillix.com>
2025-05-14 11:48:53 +0200

    update v25

---
+++ Full-Stack Web Developer_PROJECT/SECTION_19_JQUERY/1.Manipulating Styles with jQuery.js
@@ -0,0 +1,2 @@
+console.log($("h1").css("color"));
+$("button");
--- Full-Stack Web Developer_PROJECT/SECTION_19_JQUERY/JQuery/index.js
+++ Full-Stack Web Developer_PROJECT/SECTION_19_JQUERY/JQuery/index.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-    $("h1").css("color", "red");
-})
+// $(document).ready(function(){
+//     $("h1").css("color", "red");
+// })
+
+
+// console.log($("h1").css("color", "#ff00d3ff"));
+
+//font style from class 
+$("h1").addClass("big-title");
+// console.log($("h1").css("font-size", "3rem"));
+
+
