04e8d9f83e26e8c3ea9fdcdf1a132061133aa72f
Dan Kibwika <dkibwika@akillix.com>
2025-06-07 09:14:09 +0200

    update v43

--- Full-Stack Web Developer_PROJECT/SECTION_24/1.EJS/4.2+Passing+Data/index.js
+++ Full-Stack Web Developer_PROJECT/SECTION_24/1.EJS/4.2+Passing+Data/index.js
@@ -6,9 +6,18 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const fnameLetter = req.body["fName"].length;
+  const lnameLetter = req.body["lName"].length;
+
+  let fullLength = fnameLetter + lnameLetter;
+  res.render("index.ejs", {numLetters: fullLength})
+  console.log(fullLength);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
